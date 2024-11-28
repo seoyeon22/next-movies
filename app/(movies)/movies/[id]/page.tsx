cb197b6e99d0b50b10def29e55c36e7e8318b786
Seoyeon Hong <hsy2709@gmail.com>
2024-11-28 21:14:25 +0900

    Initial commit

---
+++ app/(movies)/movies/[id]/page.tsx
@@ -0,0 +1,22 @@
+import { Suspense } from "react";
+import MovieVideos from "../../../../components/movie-videos";
+import MovieInfo from "../../../../components/movie-info";
+
+export default async function MovieDetail({
+    params,
+}: {
+    params: { id: string };
+}) {
+    const { id } =  await params;
+    return (
+        <div>
+            <h3>Movie detail page</h3>
+            <Suspense fallback={<h1>Loading movie info</h1>}>
+                <MovieInfo id={id}/>
+            </Suspense>
+            <Suspense fallback={<h1>Loading movie videos</h1>}>
+                <MovieVideos id={id}/>
+            </Suspense>
+        </div>
+    );
+}
