d0cd5531ec1fd93a87fd1d7e74fbbda9c9036a45
Seoyeon Hong <hsy2709@gmail.com>
2024-11-29 17:15:30 +0900

    modify params

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -2,18 +2,22 @@ import { Suspense } from "react";
 import MovieVideos from "../../../../components/movie-videos";
 import MovieInfo, { getMovie } from "../../../../components/movie-info";
 
-interface IParams {
-    params: { id: string }; 
-}
+type IParams = Promise<{
+    id: string;
+}>;
 
-export async function generateMetadata({params:{ id }}: IParams){
+export async function generateMetadata(props: { params: IParams }){
+    const params = await props.params;
+    const id = params.id;
     const movie = await getMovie(id);
     return {
         title: movie.title,
     };
 }
 
-export default async function MovieDetail({ params: { id }} : IParams) {
+export default async function MovieDetail(props: { params: IParams }) {
+    const params = await props.params;
+    const id = params.id;
     return (
         <div>
             <Suspense fallback={<h1>Loading movie info</h1>}>
